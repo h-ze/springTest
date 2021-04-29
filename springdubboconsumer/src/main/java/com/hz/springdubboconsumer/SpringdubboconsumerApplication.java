433f033b9fe3d9360f44bc1e8a0d3913d1c1e044
hz <hz15858@163.com>
2021-04-29 23:42:06 +0800

    update

--- springdubboconsumer/src/main/java/com/hz/springdubboconsumer/SpringdubboconsumerApplication.java
+++ springdubboconsumer/src/main/java/com/hz/springdubboconsumer/SpringdubboconsumerApplication.java
@@ -2,8 +2,9 @@ package com.hz.springdubboconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class SpringdubboconsumerApplication {
 
     public static void main(String[] args) {
--- springdubboconsumer/src/main/java/com/hz/springdubboconsumer/dubbo/UserInterfaceConsumer.java
+++ springdubboconsumer/src/main/java/com/hz/springdubboconsumer/dubbo/UserInterfaceConsumer.java
@@ -2,13 +2,18 @@ package com.hz.springdubboconsumer.dubbo;
 
 
 import com.alibaba.dubbo.config.annotation.Reference;
-import com.alibaba.dubbo.config.annotation.Service;
-import org.apache.catalina.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/test")
 public class UserInterfaceConsumer {
+
     @Reference(version = "1.0.0")
     private UserInterface userInterface;
 
+    @GetMapping("/test")
     public void getUserController(){
         userInterface.getUser();
     }
